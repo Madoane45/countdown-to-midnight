5832d783d0ab543b6e930c705e5612bd8cee68c4
Madoane45 <Madoane45@gmail.com>
2021-02-04 21:57:25 -0500

    commit tree 2adf4e3d22bcbf50c3016e83a4bba0ff9ac5646d

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
-
-def countdown
+require "pry"
+def countdown (number)
+    while number > 0
+        #binding.pry
+        puts "#{number} SECOND(S)!"
+        number -= 1   
+    end
+    return "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep (number)
+    while number > 0
+     sleep(1)
+        puts "#{number} SECOND(S)!"
+        number -= 1   
+    end
+    return "HAPPY NEW YEAR!"
 end
